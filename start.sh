75fb78283bc1bf69c544625b879cb3bd537927e3
David <youchen.du@gmail.com>
2017-04-25 17:46:13 +0800

    [update] Run a infinity loop to crawl.

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ echo "Starting maintainer..."
 nohup python3 RedisDaemon/maintainer.py >/dev/null 2>&1 &
 cd ProxyCrawl
 echo "Starting crawler..."
-nohup scrapy crawlall >../crawler.log 2>&1 &
+nohup bash -c "scrapy crawlall" >../crawler.log 2>&1 &
