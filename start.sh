be7ac07e67c8c826b945da8f43fbd222104da779
David <youchen.du@gmail.com>
2017-04-25 17:44:18 +0800

    [update] redirect maintain log to /dev/null.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting maintainer..."
-nohup python3 RedisDaemon/maintainer.py >maintain.log 2>&1 &
+nohup python3 RedisDaemon/maintainer.py >/dev/null 2>&1 &
 cd ProxyCrawl
 echo "Starting crawler..."
 nohup scrapy crawlall >../crawler.log 2>&1 &
