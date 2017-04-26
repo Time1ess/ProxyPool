7528cab1877f9fdf7cf5e1c3a80bdc39762cbd5f
David <youchen.du@gmail.com>
2017-04-26 10:28:26 +0800

    [fix] Fix Python output to log file.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting maintainer..."
-nohup python3 RedisDaemon/maintainer.py >maintain.log 2>&1 &
+nohup python3 -u RedisDaemon/maintainer.py >maintain.log 2>&1 &
 cd ProxyCrawl
 echo "Starting crawler..."
 nohup bash -c "while true;do scrapy crawlall;done" >../crawler.log 2>&1 &
